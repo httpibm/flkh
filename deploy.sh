4ba8c632f5760cee4c7090946722e2ac7f7d31c5
httpibm <111684910+httpibm@users.noreply.github.com>
2022-08-21 19:16:05 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,7 @@ fi
 
 # set default env
 IBM_MEMORY=${IBM_MEMORY:-"128M"}
-V2_ID=${V2_ID:-"d007eab8-ac2a-4a7f-287a-f0d50ef08680"}
+V2_ID=${V2_ID:-"d007eab8-ac2a-4a7f-287a-f7d50ef08680"}
 V2_PATH=${V2_PATH:-"path"}
 ALTER_ID=${ALTER_ID:-"1"}
 VLESS_EN=${VLESS_EN:-"false"}
@@ -86,7 +86,7 @@ if [ ! -f "$CF" ]; then
 fi
 
 echo "${BLUE}cf login${END}"
-$CF login -a https://api.us-south.cf.cloud.ibm.com <<EOF
+$CF login -a https://api.eu-de.cf.cloud.ibm.com <<EOF
 $IBM_ACCOUNT
 EOF
 
